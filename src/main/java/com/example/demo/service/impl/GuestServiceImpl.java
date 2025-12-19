2e03f5f11e389142c90160a0fb300afd888f0ae8
bala-amypo <bala@amypo.in>
2025-12-19 16:59:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/GuestServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Guest;
+import com.example.demo.repository.GuestRepository;
+import com.example.demo.service.GuestService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GuestServiceImpl implements GuestService {
+
+    private final GuestRepository guestRepository;
+
+    public GuestServiceImpl(GuestRepository guestRepository) {
+        this.guestRepository = guestRepository;
+    }
+
+    @Override
+    public Guest createGuest(Guest guest) {
+        if (guestRepository.existsByEmail(guest.getEmail())) {
+            throw new IllegalArgumentException("Email already");
+        }
+        return guestRepository.save(guest);
+    }
+
+    @Override
+    public Guest updateGuest(Long id, Guest guest) {
+        Guest existing = guestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Guest not found"));
+
+        existing.setFullName(guest.getFullName());
+        existing.setPhoneNumber(guest.getPhoneNumber());
+        existing.setVerified(guest.getVerified());
+        existing.setActive(guest.getActive());
+        existing.setRole(guest.getRole());
+
+        return guestRepository.save(existing);
+    }
+
+    @Override
+    public Guest getGuestById(Long id) {
+        return guestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Guest not found"));
+    }
+
+    @Override
+    public List<Guest> getAllGuests() {
+        return guestRepository.findAll();
+    }
+
+    @Override
+    public void deactivateGuest(Long id) {
+        Guest guest = getGuestById(id);
+        guest.setActive(false);
+        guestRepository.save(guest);
+    }
+}
