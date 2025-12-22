a765d02f96032a488fe68e4f90a8c58a89c76a91
bala-amypo <bala@amypo.in>
2025-12-22 09:45:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/GuestController.java
+++ src/main/java/com/example/demo/controller/GuestController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
+@CrossOrigin(origins = "*")
+
+
+
 @RestController
 @RequestMapping("/api/guests")
 public class GuestController {
