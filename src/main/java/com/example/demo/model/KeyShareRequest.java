3cbd5908a82e4f0142ea78edf0c7294dbadae686
bala-amypo <bala@amypo.in>
2025-12-26 16:19:41 +0000

    User commit

--- src/main/java/com/example/demo/model/KeyShareRequest.java
+++ src/main/java/com/example/demo/model/KeyShareRequest.java
@@ -1,13 +1,25 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
 import java.time.Instant;
 
+@Entity
+@Table(name = "key_share_requests")
 public class KeyShareRequest {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private DigitalKey digitalKey;
+
+    @ManyToOne
     private Guest sharedBy;
+
+    @ManyToOne
     private Guest sharedWith;
+
     private Instant shareStart;
     private Instant shareEnd;
 
