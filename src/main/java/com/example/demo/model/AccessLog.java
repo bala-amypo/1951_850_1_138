1a6027a34083eadfcdd63f925d68f0b56f942042
bala-amypo <bala@amypo.in>
2025-12-26 16:40:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AccessLog.java
@@ -0,0 +1,60 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+
+@Entity
+public class AccessLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private DigitalKey digitalKey;
+
+    @ManyToOne
+    private Guest guest;
+
+    private Instant accessTime;
+    private String result;
+
+    public AccessLog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public DigitalKey getDigitalKey() {
+        return digitalKey;
+    }
+
+    public void setDigitalKey(DigitalKey digitalKey) {
+        this.digitalKey = digitalKey;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public Instant getAccessTime() {
+        return accessTime;
+    }
+
+    public void setAccessTime(Instant accessTime) {
+        this.accessTime = accessTime;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
