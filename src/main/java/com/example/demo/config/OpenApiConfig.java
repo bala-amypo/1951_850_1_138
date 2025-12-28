6838a518f3a670cfad9a38d4d387e652bfbd1fd4
bala-amypo <bala@amypo.in>
2025-12-28 06:29:58 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,22 +1,43 @@
 package com.example.demo.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "BearerAuth";
+
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Hotel Room Key Digital Share API")
+                        .version("1.0"))
                 .servers(List.of(
                         new Server().url("https://9252.408procr.amypo.ai/")
-                ));
-        }
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
 }
